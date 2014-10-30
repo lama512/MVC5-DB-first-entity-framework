3ce5d2dafece82bf5978f777d863768ee99f206a
lama512 <lamloom@live.com>
2014-10-30 09:05:11 +0200

    all about and MVC5 demo
    all about and MVC5 demo

---
+++ task/task/Controllers/categoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class categoryController : Controller
+    {
+        DataEntry db = new DataEntry();
+        public ActionResult Index()
+        {
+            return View(db.categories.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+    }
+}
---
+++ task/task/Controllers/datumController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task.Controllers
+{
+    public class datumController : Controller
+    {
+        DataEntry db = new DataEntry();
+        public ActionResult Index()
+        {
+            var data = from p in db.categories
+
+                       select p.categoryName;
+
+            SelectList list = new SelectList(data);
+            ViewBag.Roles = list;
+            return View();
+        }
+        public ActionResult create ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            string category = Request.Form["category"];
+
+            if (file != null)
+            {
+                
+               string ImageName = System.IO.Path.GetFileName(file.FileName);
+               // string physicalPath = Server.MapPath("~/images/" + ImageName);
+              //  string physicalPath = "~/" + category + "/" + ImageName; // your code goes here
+                var path = Path.Combine(Server.MapPath("~/images/" + category), ImageName);
+                string subpath = "~/images/" + category;
+   
+
+                bool exists = System.IO.Directory.Exists(Server.MapPath(subpath));
+
+                if (!exists) {
+                    System.IO.Directory.CreateDirectory(Server.MapPath(subpath));
+                }
+                   
+                                
+
+                // save image in folder
+                file.SaveAs(path);
+                
+
+                //save new record in database
+               datum newRecord = new datum();
+                newRecord.category = Request.Form["category"];
+                newRecord.description = Request.Form["description"];
+                newRecord.imagePath = ImageName;
+                db.data.Add(newRecord);
+                db.SaveChanges();
+               
+
+            }
+            //Display records
+            return RedirectToAction("Display");
+        }
+
+        public ActionResult Display()
+        {
+            return View();
+        }
+	}
+}
